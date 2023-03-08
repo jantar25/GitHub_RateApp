b7fdbbac2ecd13a4217596568203ef4fe58c2dd2
unknown <glodybulambo@gmail.com>
2023-03-08 02:26:59 +0200

    the sign-in form

---
+++ src/Components/FormikTextInput.jsx
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { useField } from 'formik';
+
+import TextInput from './TextInput';
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  errorText: {
+    marginTop: 5,
+  },
+  input: {
+    marginVertical:10,
+    padding:10,
+    borderWidth: 1,
+    borderColor: theme.colors.textSecondary,
+    borderRadius:5
+  }
+});
+
+const FormikTextInput = ({ name, ...props }) => {
+  const [field, meta, helpers] = useField(name);
+  const showError = meta.touched && meta.error;
+
+  return (
+    <>
+      <TextInput
+        onChangeText={value => helpers.setValue(value)}
+        onBlur={() => helpers.setTouched(true)}
+        value={field.value}
+        error={showError}
+        style={styles.input}
+        {...props}
+      />
+      {showError && <Text style={styles.errorText}>{meta.error}</Text>}
+    </>
+  );
+};
+
+export default FormikTextInput;
--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,7 +1,52 @@
+import { View, Pressable,StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 import Text from './Text';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const LogInForm = ({onSubmit}) => {
+  const styles = StyleSheet.create({
+    form: {
+      backgroundColor:'#fff',
+      padding:20
+    },
+    button: {
+      marginTop:10,
+      paddingVertical:10,
+      textAlign:'center',
+      backgroundColor:theme.colors.primary,
+      color:'#fff',
+      borderRadius:5
+    }
+  });
+
+  return (
+    <View style={styles.form}>
+    <FormikTextInput name="username" placeholder="Username" />
+    <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+    <Pressable onPress={onSubmit}>
+      <Text style={styles.button}>Sign In</Text>
+    </Pressable>
+  </View>
+  )
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <LogInForm onSubmit={handleSubmit} />}
+    </Formik>
+
+  );
 };
 
 export default SignIn;
