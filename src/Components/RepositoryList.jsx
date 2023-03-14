ac43c8ed672f5ad3599ffe90732f7501dc9e44a6
unknown <glodybulambo@gmail.com>
2023-03-15 01:20:40 +0200

    useRepository hooks work

--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 // import { useQuery } from '@apollo/client';
 
-// import useRepositories from '../Hooks/useRepositories';
+import useRepositories from '../Hooks/useRepositories';
 import Item from './RepositoryItem';
 // import { GET_REPOSITORIES } from '../graphql/queries';
 
@@ -12,74 +11,10 @@ const styles = StyleSheet.create({
   },
 });
 
-// const repositories = [
-//   {
-//     id: 'jaredpalmer.formik',
-//     fullName: 'jaredpalmer/formik',
-//     description: 'Build forms in React, without the tears',
-//     language: 'TypeScript',
-//     forksCount: 1589,
-//     stargazersCount: 21553,
-//     ratingAverage: 88,
-//     reviewCount: 4,
-//     ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
-//   },
-//   {
-//     id: 'rails.rails',
-//     fullName: 'rails/rails',
-//     description: 'Ruby on Rails',
-//     language: 'Ruby',
-//     forksCount: 18349,
-//     stargazersCount: 45377,
-//     ratingAverage: 100,
-//     reviewCount: 2,
-//     ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
-//   },
-//   {
-//     id: 'django.django',
-//     fullName: 'django/django',
-//     description: 'The Web framework for perfectionists with deadlines.',
-//     language: 'Python',
-//     forksCount: 21015,
-//     stargazersCount: 48496,
-//     ratingAverage: 73,
-//     reviewCount: 5,
-//     ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
-//   },
-//   {
-//     id: 'reduxjs.redux',
-//     fullName: 'reduxjs/redux',
-//     description: 'Predictable state container for JavaScript apps',
-//     language: 'TypeScript',
-//     forksCount: 13902,
-//     stargazersCount: 52869,
-//     ratingAverage: 0,
-//     reviewCount: 0,
-//     ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
-//   },
-// ];
-
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-  // const { data, error, loading } = useQuery(GET_REPOSITORIES);
-
-  const fetchRepositories = async () => {
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.1.33:5000/api/repositories');
-    const json = await response.json();
-
-    console.log(json);
-
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  // const { repositories } = useRepositories();
+  const { repositories } = useRepositories();
 
   // Get the nodes from the edges array
   const repositoryNodes = repositories
