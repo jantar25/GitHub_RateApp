e840b6cf90553319a462b3f3c373e4f9cd541733
unknown <glodybulambo@gmail.com>
2023-03-19 01:58:06 +0200

    example test working

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -21,17 +21,17 @@ const LogInForm = ({onSubmit}) => {
       backgroundColor:theme.colors.primary,
       color:'#fff',
       borderRadius:5
-    }
+    },
   });
 
   return (
     <View style={styles.form}>
-    <FormikTextInput name="username" placeholder="Username" />
-    <FormikTextInput name="password" placeholder="Password" secureTextEntry />
-    <Pressable onPress={onSubmit}>
-      <Text style={styles.button}>Sign In</Text>
-    </Pressable>
-  </View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+      <Pressable onPress={onSubmit}>
+        <Text style={styles.button}>Sign In</Text>
+      </Pressable>
+    </View>
   )
 }
 
